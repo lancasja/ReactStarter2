1283f3034597b7b448b7b73886f6952c6be344fa
lancasja <lancasja@gmail.com>
2015-09-22 02:59:54 -0700

    Removed invalid characters not seen from last merge conflict.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,6 @@
 /* Main */
 var React = require('react');
 var Routes = require('./routes');
-<<<<<<< HEAD
-=======
-var Api = require('./utils/api');
->>>>>>> f8200e0e88140d04cf32f4934b5be3e0e8ec89b7
 
 React.render(Routes, document.querySelector('.container'));
 
